4b8af1dfc80d8384a10edba4877aaa7d82735859
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-05 16:56:47 +0100

    feat : update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
@@ -63,5 +64,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the role associated with the user.
+     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
     }
 }
