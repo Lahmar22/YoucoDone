ad58483a5ca02cdf5895d9d7054ec8da7220694c
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-05 14:53:21 +0100

    feat : add model the menu

---
+++ app/Models/Menu.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Menu extends Model
+{
+    protected $fillable = [
+        'name',
+        'restaurant_id',
+        'image',
+    ];
+
+    /**
+     * Get all items for this menu
+     */
+    public function items()
+    {
+        return DB::table('mmi')
+            ->join('menu_item', 'mmi.menu_item_id', '=', 'menu_item.id')
+            ->where('mmi.menu_id', $this->id)
+            ->select('menu_item.*')
+            ->get();
+    }
+
+    /**
+     * Get menu items count
+     */
+    public function getItemsCountAttribute()
+    {
+        return DB::table('mmi')
+            ->where('menu_id', $this->id)
+            ->count();
+    }
+}
