7e0ca08010297d9ab6817cc3c040524c7c7fc29a
Lahmar22 <lahmarzakariae22@gmail.com>
2026-02-05 15:23:08 +0100

    feat : update table users in migration

--- database/migrations/2026_02_05_141405_add_name_to_role_table.php
+++ database/migrations/2026_02_05_141405_add_name_to_role_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role', function (Blueprint $table) {
-            $table->id();
-            $table->string('description')->nullable();
-
-            $table->timestamps();
+        Schema::table('role', function (Blueprint $table) {
+            $table->string('name')->after('id');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role');
+        Schema::table('role', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
     }
 };
--- database/migrations/2026_02_05_141803_add_role_id_to_users_table.php
+++ database/migrations/2026_02_05_141803_add_role_id_to_users_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role', function (Blueprint $table) {
-            $table->id();
-            $table->string('description')->nullable();
-
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained('role')->after('id');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+        });
     }
 };
